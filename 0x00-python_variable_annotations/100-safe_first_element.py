0a53852f777472297a7270592ce24f024fff6b83
Ayowale <haydexwisdom@gmail.com>
2024-01-11 20:30:29 +0100

    Task 10 done

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+100-safe_first_element module
+"""
+from typing import Sequence, Union, Any
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """
+    returns list or None
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
