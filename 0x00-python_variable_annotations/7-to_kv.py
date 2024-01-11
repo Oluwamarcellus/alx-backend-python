0cd98655b883b5c7f61e3c5dd13ba73e1b3b70cb
Ayowale <haydexwisdom@gmail.com>
2024-01-11 19:45:15 +0100

    Task 7 done

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,7 +5,7 @@ to_kv module
 from typing import Union
 
 
-def to_kv(k: str, v: (int | float)) -> tuple[str, Union[(int, float)]]:
+def to_kv(k: str, v: Union[int, float]) -> tuple[str, Union[int, float]]:
     """
     a type-annotated function to_kv that takes a string k and an
     int OR float v as arguments and returns a tuple
