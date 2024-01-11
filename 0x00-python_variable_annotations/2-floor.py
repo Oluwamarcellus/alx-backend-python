b40e633ec3ba9a69cbf152fced524b6597f7fb64
Ayowale <haydexwisdom@gmail.com>
2024-01-11 18:56:46 +0100

    Task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+floor module
+"""
+import math
+
+
+def floor(n: float) -> float:
+    """
+    a type-annotated function floor which takes a float n as
+    argument and returns the floor of the float.
+    """
+    return math.floor(n)
